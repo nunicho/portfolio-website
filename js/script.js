09d8ed699fad4726296076fc6feaf89883a83839
MauricioAlonso <program.alonso@gmail.com>
2025-07-31 21:37:12 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Header
+
+function runTypingEffect() {
+  const text = "I am Mauricio Alonso.";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 100; // milliseconds
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+        typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect)
